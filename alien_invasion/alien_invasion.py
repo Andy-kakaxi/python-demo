db2f29b97a8a2d24e2f79cf6b7d225ecfaad6718
Andy-kakaxi <kakaximovie@163.com>
2019-03-23 16:09:03 +0800

    1.创建游戏设置模块：settings 2.创建飞船类：ship 3.更新游戏主程序，使用设置模块和飞船模块配置游戏

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,14 +4,18 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 def run_game():
-    # 初始化游戏并创建一个屏幕对象
+    # 初始化pygame、设置和屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 设置背景颜色
-    bg_color = (230,230,230)
+    #创建一艘飞船
+    ship = Ship(screen)
 
     # 开始游戏的主循环
     while True:
@@ -22,7 +26,8 @@ def run_game():
                 sys.exit()
 
         # 每次循环时都重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
---
+++ alien_invasion/settings.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+class Settings():
+    """存储《外星人入侵》的所有设置类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕的设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+
