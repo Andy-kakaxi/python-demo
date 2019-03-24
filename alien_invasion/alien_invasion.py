9e6dd194425979adf2c40c2153239d094809c0b9
Andy-kakaxi <kakaximovie@163.com>
2019-03-24 16:46:18 +0800

    1.外星人开发完成

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,10 +2,11 @@
 
 import pygame
 from pygame.sprite import Group
-
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from game_stats import GameStats
+from button import Button
 
 def run_game():
     # 初始化pygame、设置和屏幕对象
@@ -14,17 +15,28 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 创建一艘飞船
-    ship = Ship(ai_settings,screen)
-    # 创建一个用于存储子弹的编组
-    bullets = Group()
+    # 创建Play按钮
+    play_button = Button(ai_settings,screen,"Play")
 
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
+    ship = Ship(ai_settings,screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings,screen,ship,aliens)
 
     # 开始游戏的主循环
     while True:
-        gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.check_events(ai_settings,screen,stats,play_button,ship,bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+
+        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
 
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -12,13 +12,20 @@ class Settings():
 
         # 飞船的设置
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
 
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 1
         self.bullet_height = 15
         self.bullet_color = 60,60,60
         self.bullets_allowed = 3
+
+        #外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        #feet_direction为1表示向右移动，为-1表示向左移动
+        self.fleet_direction = 1
 
 
 
