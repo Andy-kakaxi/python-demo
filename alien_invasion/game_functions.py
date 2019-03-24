c0ba1fb44927acfe02d040cce58a64da5941a12d
Andy-kakaxi <kakaximovie@163.com>
2019-03-25 07:16:00 +0800

    1.开发计分模块

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -32,7 +32,7 @@ def check_keyup_events(event,ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings,screen,stats,play_button,ship,bullets):
+def check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -46,11 +46,31 @@ def check_events(ai_settings,screen,stats,play_button,ship,bullets):
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x,mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats,play_button,mouse_x,mouse_y)
-def check_play_button(stats,play_button,mouse_x,mouse_y):
+            check_play_button(ai_settings,screen,stats,play_button,ship,
+                              aliens,bullets,mouse_x,mouse_y)
+
+def check_play_button(ai_settings,screen,stats,play_button,ship,aliens,
+                      bullets,mouse_x,mouse_y):
     """在玩家单击Play按钮时开始新游戏"""
-    if play_button.rect.collidepoint(mouse_x,mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x,mouse_y)
+    if button_clicked and not stats.game_active:
+        # 重置游戏设置
+        ai_settings.initialize_dynamic_settings()
+
+        # 隐藏光标
+        pygame.mouse.set_visible(False)
+
+        # 重置统计信息
+        stats.reset_stats()
         stats.game_active = True
+
+        # 清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty()
+
+        # 创建一群新的外星人，并让飞船居中
+        create_fleet(ai_settings,screen,ship,aliens)
+        ship.center_ship()
 
 def update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button):
     """更新屏幕上的图像，并切换到新屏幕"""
@@ -86,8 +106,9 @@ def check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-        # 删除现有的子弹并新建一群外星人
+        # 删除现有的子弹，加快游戏节奏，并创建一群新的外星人
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings,screen,ship,aliens)
 
 def get_number_aliens_x(ai_settings,alien_width):
@@ -144,6 +165,7 @@ def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 def update_aliens(ai_settings,stats,screen,ship,aliens,bullets):
     """检查是否有外星人位于屏幕边缘，并更新整群外星人的位置"""
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,7 +4,7 @@ class Settings():
     """存储《外星人入侵》的所有设置类"""
 
     def __init__(self):
-        """初始化游戏的设置"""
+        """初始化游戏的静态设置"""
         # 屏幕的设置
         self.screen_width = 1200
         self.screen_height = 800
@@ -21,11 +21,30 @@ class Settings():
         self.bullet_color = 60,60,60
         self.bullets_allowed = 3
 
-        #外星人设置
+        # 外星人设置
         self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
-        #feet_direction为1表示向右移动，为-1表示向左移动
+        # feet_direction为1表示向右移动，为-1表示向左移动
         self.fleet_direction = 1
 
+        # 以什么样的速度加快游戏节奏
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction为1表示向右；为-1表示向左
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """提高速度设置"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
 
 
