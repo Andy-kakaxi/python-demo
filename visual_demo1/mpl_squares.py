3a4e9b4bf74cf5c63562dcb89306476fbe01c89d
Andy-kakaxi <kakaximovie@163.com>
2019-03-29 21:56:19 +0800

    1.了解plot,scatter函数用法。

--- visual_demo1/mpl_squares.py
+++ visual_demo1/mpl_squares.py
@@ -2,6 +2,36 @@
 
 import matplotlib.pyplot as plt
 
-squares = [1,4,9,16,25]
-plt.plot(squares)
+x_values = list(range(1,1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values,y_values,c=y_values,
+            cmap=plt.cm.Blues,edgecolors='none',s=40)
+
+# 设置图表标题并给坐标轴加上标签
+plt.title("Square Numbers",fontsize=24)
+plt.xlabel("Value",fontsize=14)
+plt.ylabel("Square of Value",fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis='both',which='major',
+                labelsize=14)
+
+# 设置每个坐标轴的取值范围
+plt.axis([0,1100,0,1100000])
+
 plt.show()
+
+
+# input_values = [1,2,3,4,5]
+# squares = [1,4,9,16,25]
+# plt.plot(input_values,squares,linewidth=5)
+
+# 设置图标标题，并给坐标轴加上标签
+# plt.title("Square Numbers",fontsize=24)
+# plt.xlabel("Value",fontsize=14)
+# plt.ylabel("Square of Value",fontsize=14)
+
+# 设置刻度标记的大小
+# plt.tick_params(axis='both',labelsize=14)
+# plt.show()
