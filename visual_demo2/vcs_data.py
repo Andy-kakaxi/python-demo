d1fa70430b199f2c6ac81ec700a5c1f36607e2cf
Andy-kakaxi <kakaximovie@163.com>
2019-03-31 16:56:50 +0800

    1.visual_demo2

---
+++ visual_demo2/vcs_data.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+# 从文件中获取日期、最高气温和最低气温
+filename = 'sitka_weather_2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates,highs,lows = [],[],[]
+    for row in reader:
+        current_date = datetime.strptime(row[0],"%Y-%m-%d")
+        dates.append(current_date)
+        high = int(row[1])
+        highs.append(high)
+
+        low = int(row[3])
+        lows.append(low)
+
+# 根据数据绘制图形
+fig = plt.figure(dpi=128,figsize=(10,6))
+plt.plot(dates,highs,c='red',alpha=0.5)
+plt.plot(dates,lows,c='blue',alpha=0.5)
+plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
+
+# 设置图形的格式
+plt.title("Daily high and low temperatures - 2014",fontsize=24)
+plt.xlabel('',fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)",fontsize=16)
+plt.tick_params(axis='both',which='major',labelsize=16)
+
+plt.show()
