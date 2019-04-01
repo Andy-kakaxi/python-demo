6527e853ed66d5452c0dc9301f659ced8bfc5a3c
Andy-kakaxi <kakaximovie@163.com>
2019-04-01 21:13:20 +0800

    更新"visual_demo3"项目文件

--- visual_demo3/btc_close_2017.py
+++ visual_demo3/btc_close_2017.py
@@ -76,8 +76,8 @@ with open('收盘价Dashboard.html', 'w', encoding='utf8') as html_file:
     html_file.write(
         '<html><head><title>收盘价Dashboard</title><meta charset="utf-8"></head><body>\n')
     for svg in [
-            '收盘价折线图（¥）.svg', '收盘价对数变换折线图（¥）.svg', '收盘价月日均值（¥）.svg',
-            '收盘价周日均值（¥）.svg', '收盘价星期均值（¥）.svg'
+            '收盘价折线图（￥）.svg', '收盘价对数变换折线图（￥）.svg', '收盘价月日均值（￥）.svg',
+            '收盘价周日均值（￥）.svg', '收盘价星期均值（￥）.svg'
     ]:
         html_file.write(
             '    <object type="image/svg+xml" data="{0}" height=500></object>\n'.format(svg))  # 1
