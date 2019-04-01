ac9b0b2726d5d2c22a66a77a6673f71fb355edfc
Andy-kakaxi <kakaximovie@163.com>
2019-04-01 21:43:46 +0800

    API调用及数据可视化

--- web_application/python_repos.py
+++ web_application/python_repos.py
@@ -1,14 +1,31 @@
 # -*- coding:utf-8 -*-
 
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # 执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
-print("Status code:",r.status_code)
+print("Status code:", r.status_code)
 
 # 将API响应存储在一个变量中
 response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
 
-# 处理结果
-print(response_dict.keys())
+# 探索有关仓库的信息
+repo_dicts = response_dict['items']
+
+names,stars = [],[]
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# 可视化
+my_style = LS('#333366',base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
