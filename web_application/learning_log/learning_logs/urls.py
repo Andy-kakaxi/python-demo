2c3712622b592a88e2501bbe5f9b8a95fc9262b9
Andy-kakaxi <kakaximovie@163.com>
2019-04-08 23:13:22 +0800

    django项目Demo

---
+++ web_application/learning_log/learning_logs/urls.py
@@ -0,0 +1,31 @@
+"""
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+"""定义learning_logs的URL模式"""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # 主页
+    path(r'', views.index, name='index'),
+
+    # 显示所有的主题
+    path(r'topics/', views.topics, name='topics'),
+
+    # 特定主题的详细页面
+    path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+]
---
+++ web_application/learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/science/PycharmProjects/python-demo/web_application/learning_log/ll_env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
