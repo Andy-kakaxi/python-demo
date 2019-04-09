553d27a87e5c0cec8ac43c5a52cd83e3dc2b9ce1
Andy-kakaxi <kakaximovie@163.com>
2019-04-09 23:01:33 +0800

    添加新增学习条目功能。

--- web_application/learning_log/learning_logs/urls.py
+++ web_application/learning_log/learning_logs/urls.py
@@ -28,4 +28,10 @@ urlpatterns = [
 
     # 特定主题的详细页面
     path('topics/<topic_id>/', views.topic, name='topic'),
+
+    # 用于添加新主题的网页
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # 用于添加新条目的页面
+    path('new_entry/<topic_id>', views.new_entry, name='new_entry'),
 ]
