ed7dad57c361cbb258ee87da566736bc97e49419
Andy-kakaxi <kakaximovie@163.com>
2019-05-13 22:14:59 +0800

    调整csv示例代码格式

--- visual_demo2/csv_data.py
+++ visual_demo2/csv_data.py
@@ -11,31 +11,31 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    dates,highs,lows = [],[],[]
+    dates, highs, lows = [], [], []
     for row in reader:
         try:
-            current_date = datetime.strptime(row[0],"%Y-%m-%d")
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
             high = int(row[1])
             low = int(row[3])
         except ValueError:
-            print(current_date,'missing data')
+            print(current_date, 'missing data')
         else:
             dates.append(current_date)
             highs.append(high)
             lows.append(low)
 
 # 根据数据绘制图形
-fig = plt.figure(dpi=128,figsize=(10,6))
-plt.plot(dates,highs,c='red',alpha=0.5)
-plt.plot(dates,lows,c='blue',alpha=0.5)
-plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # 设置图形的格式
 title = "Daily high and low temperatures - 2014"
-plt.title(title,fontsize=24)
-plt.xlabel('',fontsize=16)
+plt.title(title, fontsize=24)
+plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
-plt.ylabel("Temperature (F)",fontsize=16)
-plt.tick_params(axis='both',which='major',labelsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both',  which='major', labelsize=16)
 
 plt.show()
